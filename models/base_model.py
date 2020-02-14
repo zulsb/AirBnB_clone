fde90e711f797a5dc832d21917b0e7cb2e6548ae
agzsoftsi <agzsoftsi@gmail.com>
2020-02-14 20:54:25 +0000

    update code

--- models/base_model.py
+++ models/base_model.py
@@ -12,19 +12,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Initialize Base Model instance. """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if kwargs:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             for key, val in kwargs.items():
-                if key == "created_at":
-                    self.created_at = datetime.strptime(kwargs["created_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                else:
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key,
+                            datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif not key == "__class__":
                     setattr(self, key, val)
 
     def __str__(self):
