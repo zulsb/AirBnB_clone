c9269d8099130b34e1f0858c533a35b4bb055cca
agzsoftsi <agzsoftsi@gmail.com>
2020-02-14 23:01:12 +0000

    Update Task 6 - console

--- console.py
+++ console.py
@@ -2,3 +2,25 @@
 """
     Module contains the entry to command interpreter.
 """
+import cmd
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class"""
+    prompt = '(hbnb)'
+
+    def do_quit(self, *args):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, *args):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """If this method is not overridden, repeats the last
+            nonempty command entered"""
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
