0ba67fea40cd4766309bba61d5466506393d634f
Luz <zulsb2093@gmail.com>
2020-02-14 20:39:15 +0000

    4 task

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python3
 """
-    Module define the BaseModel class.
-    defines all common attributes/methods for other classes
+    Module define the BaseModel class and defines all
+    common attributes/methods for other classes.
 """
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """ class Base Model"""
+    """ Class Base Model. """
 
-    def __init__(self):
-        """ initialize Base Model instance """
+    def __init__(self, *args, **kwargs):
+        """ Initialize Base Model instance. """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    setattr(self, key, val)
+
     def __str__(self):
-        """ Prints the BaseModel Attribute of String representation"""
+        """ Prints the BaseModel Attribute of String representation. """
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
-        """ Save Method - Updates the attributes with current datetime"""
+        """ Save Method - Updates the attributes with current datetime. """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Dictionary Method-Return a dictionary of keys/values of __dict__"""
+        """ Method-Return a dictionary of keys/values of __dict__ """
         d = self.__dict__.copy()
         d["__class__"] = self.__class__.__name__
         d["updated_at"] = d["updated_at"].isoformat()
