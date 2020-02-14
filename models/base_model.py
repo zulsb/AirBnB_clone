b65679a787c1b9d633eb2870e9f9d708b39a3b76
agzsoftsi <agzsoftsi@gmail.com>
2020-02-14 21:58:32 +0000

    Update Task 5

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -16,6 +17,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
             for key, val in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -32,6 +34,7 @@ class BaseModel:
     def save(self):
         """ Save Method - Updates the attributes with current datetime. """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ Method-Return a dictionary of keys/values of __dict__ """
