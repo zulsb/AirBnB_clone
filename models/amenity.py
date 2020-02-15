c373496f1d246812441f8a5cf79c1e00014278e5
Luz <zulsb2093@gmail.com>
2020-02-15 01:30:42 +0000

    Update Task 9

--- models/amenity.py
+++ models/amenity.py
@@ -2,3 +2,12 @@
 """
     Module define the Amenity class.
 """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Amenity class inherits from BaseModel.
+        Public class attribute:
+           name: string - empty string.
+    """
+    name = ""
