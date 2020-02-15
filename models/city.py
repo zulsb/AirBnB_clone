78266e4a1478de8a9ea56c81919dbb3b67835b24
Luz <zulsb2093@gmail.com>
2020-02-15 01:26:00 +0000

    Update Task 9

--- models/city.py
+++ models/city.py
@@ -2,3 +2,14 @@
 """
     Module define the City class.
 """
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ City class inherits from BaseModel.
+        Public class attributes:
+            state_id:string will be State.id
+            name:string - empty string
+    """
+    state_id = ""
+    name = ""
